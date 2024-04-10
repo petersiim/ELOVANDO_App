98f535768e92e7c7efd289086d3494f2f8057916
Simon Peter <petersim@student.ethz.ch>
2024-04-10 11:35:44 +0200

    env variables with open ai key works now

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart' as dotenv;
+import 'package:envied/envied.dart';
+import 'env/env.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  OpenAI.apiKey = Env.apiKey;
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
