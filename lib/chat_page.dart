eda09c3e9afe98c0a6d07f662f349d0697b17a7d
Simon Peter <petersim@student.ethz.ch>
2024-04-30 10:35:45 +0200

    working transcription

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -11,7 +11,9 @@ import 'package:path_provider/path_provider.dart';
 import 'package:audioplayers/audioplayers.dart';
 
 class ChatPage extends StatefulWidget {
-  const ChatPage({Key? key, required this.title, required this.hasMicrophonePermission}) : super(key: key);
+  const ChatPage(
+      {Key? key, required this.title, required this.hasMicrophonePermission})
+      : super(key: key);
 
   final String title;
   final bool hasMicrophonePermission;
@@ -24,6 +26,8 @@ class _ChatPageState extends State<ChatPage> {
   List<String> messages = ["Descalate: How are you?"];
   final TextEditingController clientController = TextEditingController();
 
+  bool _isTranscribing = false;
+
   late Record _recorder;
   bool _isRecording = false;
 
@@ -33,7 +37,8 @@ class _ChatPageState extends State<ChatPage> {
   List<openai.OpenAIChatCompletionChoiceMessageModel> conversationHistory = [];
 
   Future<String> transcribeAudio(String filePath) async {
-    openai.OpenAIAudioModel transcription = await openai.OpenAI.instance.audio.createTranscription(
+    openai.OpenAIAudioModel transcription =
+        await openai.OpenAI.instance.audio.createTranscription(
       file: io.File(filePath),
       model: "whisper-1",
       responseFormat: openai.OpenAIAudioResponseFormat.json,
@@ -56,33 +61,42 @@ class _ChatPageState extends State<ChatPage> {
   }
 
   Future<void> stopRecording() async {
-  try {
-    await _recorder.stop();
+    try {
+      await _recorder.stop();
 
-    setState(() {
-      _isRecording = false;
-    });
+      setState(() {
+        _isRecording = false;
+      });
 
-    // Save the recording locally
-    final dir = await getApplicationDocumentsDirectory();
-    final path = '${dir.path}/rec.mp3';
+      // Save the recording locally
+      final dir = await getApplicationDocumentsDirectory();
+      final path = '${dir.path}/rec.mp3';
 
-    print('Recording saved at: $path');
+      print('Recording saved at: $path');
 
-    // Transcribe the audio
-    String transcription = await transcribeAudio(path);
-    print('Transcription: $transcription');
+      // Transcribe the audio
+      setState(() {
+        _isTranscribing = true; // Set the flag to true
+      });
 
+      String transcription = await transcribeAudio(path);
+      print('Transcription: $transcription');
 
-    // Play the recording
-    AudioPlayer audioPlayer = AudioPlayer();
-    await audioPlayer.setSource(DeviceFileSource(path));
-    await audioPlayer.resume();
-    
+      // End transcribing the audio
+      setState(() {
+        _isTranscribing = false; // Set the flag back to false
+        clientController.text =
+            transcription; // Set the transcribed text to the TextField
+      });
+
+      // Play the recording
+      AudioPlayer audioPlayer = AudioPlayer();
+      await audioPlayer.setSource(DeviceFileSource(path));
+      await audioPlayer.resume();
     } catch (e) {
-    print('Error stopping the recording: $e');
+      print('Error stopping the recording: $e');
+    }
   }
-}
 
   Future<void> initializeContextAndHistory() async {
     contextForModelTxt = await readFile();
@@ -125,7 +139,9 @@ class _ChatPageState extends State<ChatPage> {
                       margin: const EdgeInsets.only(bottom: 10),
                       padding: const EdgeInsets.all(10),
                       decoration: BoxDecoration(
-                        color: messages[index].startsWith('You: ') ? Colors.grey[200] : Colors.white,
+                        color: messages[index].startsWith('You: ')
+                            ? Colors.grey[200]
+                            : Colors.white,
                         border: Border.all(color: Colors.black),
                       ),
                       child: Text(messages[index]),
@@ -138,35 +154,57 @@ class _ChatPageState extends State<ChatPage> {
             Row(
               children: <Widget>[
                 Expanded(
-                  child: TextField(
-                    controller: clientController,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Your response',
-                    ),
-                    onSubmitted: (text) async {
-                      messages.add('You: $text');
-                      await sendMessageAndDisplay(text);
-                    },
+                  child: Stack(
+                    children: <Widget>[
+                      TextField(
+                        controller: clientController,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: _isTranscribing ? '' : 'Your response',
+                        ),
+                        onSubmitted: (text) async {
+                          messages.add('You: $text');
+                          await sendMessageAndDisplay(text);
+                        },
+                      ),
+                      if (_isTranscribing)
+                        Positioned(
+                          top: 0,
+                          bottom: 0,
+                          left:
+                              10, // Adjust this value to move the CircularProgressIndicator horizontally
+                          child: Center(
+                            child: SizedBox(
+                              width:
+                                  20, // Adjust this value to change the width of the CircularProgressIndicator
+                              height:
+                                  20, // Adjust this value to change the height of the CircularProgressIndicator
+                              child: CircularProgressIndicator(),
+                            ),
+                          ),
+                        ),
+                    ],
                   ),
                 ),
-                SizedBox(width: 10), // Add some space between the TextField and the FloatingActionButton
+                SizedBox(
+                    width:
+                        10), // Add some space between the TextField and the FloatingActionButton
                 if (widget.hasMicrophonePermission)
                   FloatingActionButton(
-  onPressed: () async {
-    if (!_isRecording) {
-      final dir = await getApplicationDocumentsDirectory();
-      final path = '${dir.path}/rec.mp3';
-      await _recorder.start(path: path);
-      setState(() {
-        _isRecording = true;
-      });
-    } else {
-      await stopRecording();
-    }
-  },
-  child: Icon(_isRecording ? Icons.stop : Icons.mic),
-),
+                    onPressed: () async {
+                      if (!_isRecording) {
+                        final dir = await getApplicationDocumentsDirectory();
+                        final path = '${dir.path}/rec.mp3';
+                        await _recorder.start(path: path);
+                        setState(() {
+                          _isRecording = true;
+                        });
+                      } else {
+                        await stopRecording();
+                      }
+                    },
+                    child: Icon(_isRecording ? Icons.stop : Icons.mic),
+                  ),
               ],
             ),
             SizedBox(height: 20),
@@ -183,36 +221,29 @@ class _ChatPageState extends State<ChatPage> {
     );
   }
 
-  
-
   Future<void> sendMessageAndDisplay(String message) async {
     clientController.clear();
-    int counter = 0;
-    Timer timer = Timer.periodic(Duration(milliseconds: 500), (Timer t) {
-      setState(() {
-        if (messages.length > 0 && messages.last.startsWith('Descalate: ')) {
-          messages.removeLast();
-        }
-        messages.add('Descalate: ' + '.' * (counter % 4));
-        counter++;
-      });
+    setState(() {
+      messages.add('Descalate: ...'); // Add a loading message
     });
     String response = await sendMessage(message);
-    timer.cancel();
     setState(() {
-      messages.removeLast();
-      messages.add('$response');
+      messages.removeLast(); // Remove the loading message
+      messages.add(response); // Add the actual response
     });
   }
 
   Future<String> sendMessage(String message) async {
     conversationHistory.add(openai.OpenAIChatCompletionChoiceMessageModel(
-      content: [openai.OpenAIChatCompletionChoiceMessageContentItemModel.text(message)],
+      content: [
+        openai.OpenAIChatCompletionChoiceMessageContentItemModel.text(message)
+      ],
       role: openai.OpenAIChatMessageRole.user,
     ));
 
     String modelInUse = "gpt-4-turbo";
-    openai.OpenAIChatCompletionModel chatCompletion = await openai.OpenAI.instance.chat.create(
+    openai.OpenAIChatCompletionModel chatCompletion =
+        await openai.OpenAI.instance.chat.create(
       model: modelInUse,
       responseFormat: {"type": "text"},
       messages: conversationHistory,
@@ -220,9 +251,14 @@ class _ChatPageState extends State<ChatPage> {
       maxTokens: 700,
     );
 
-    String responseText = chatCompletion.choices.first.message.content?.first.text ?? 'No response received';
+    String responseText =
+        chatCompletion.choices.first.message.content?.first.text ??
+            'No response received';
     conversationHistory.add(openai.OpenAIChatCompletionChoiceMessageModel(
-      content: [openai.OpenAIChatCompletionChoiceMessageContentItemModel.text(responseText)],
+      content: [
+        openai.OpenAIChatCompletionChoiceMessageContentItemModel.text(
+            responseText)
+      ],
       role: openai.OpenAIChatMessageRole.assistant,
     ));
 
