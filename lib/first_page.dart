85699c4bf5e0688f241efe8b3e55ff31e114480f
Simon Peter <petersim@student.ethz.ch>
2024-06-03 13:47:39 +0200

    looks good, next button still to be done

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -1,6 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class FirstPage extends StatelessWidget {
+class FirstPage extends StatefulWidget {
+  @override
+  _FirstPageState createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> with SingleTickerProviderStateMixin {
+  double _opacity = 0.0;
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    );
+
+    _controller.addListener(() {
+      setState(() {
+        _opacity = _controller.value;
+      });
+    });
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,11 +44,116 @@ class FirstPage extends StatelessWidget {
                 // Background image
                 Positioned.fill(
                   child: Image.asset(
-                    'graphics/bg_forest.jpg',  // Ensure the image path is correct
+                    'assets/graphics/bg_forest.jpg', // Ensure the image path is correct
                     fit: BoxFit.cover,
                   ),
                 ),
-                // Gradient overlay with 20% opacity
+                // Centered SVG logo with fade-in effect
+                Positioned(
+                  top: constraints.maxHeight * 0.31, // Adjust the top position as needed
+                  left: 0,
+                  right: 0,
+                  child: AnimatedOpacity(
+                    opacity: _opacity,
+                    duration: Duration(seconds: 1),
+                    child: SvgPicture.asset(
+                      'assets/graphics/logo_white.svg',
+                      width: constraints.maxWidth * 0.4, // Adjust size as needed
+                      height: constraints.maxHeight * 0.15, // Adjust size as needed
+                    ),
+                  ),
+                ),
+                // Bottom part
+                Positioned(
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  child: Stack(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.fromLTRB(24.0, 40.0, 24.0, 70.0), // Add padding: left, top, right, bottom
+                        decoration: BoxDecoration(
+                          color: Colors.white.withOpacity(0.8), // Slightly transparent white
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(24.0),
+                            topRight: Radius.circular(24.0),
+                          ),
+                        ),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              'Willkommen!',
+                              style: TextStyle(
+                                fontFamily: 'Inter', // Specify the font family
+                                fontSize: 48.0, // Size 48
+                                fontWeight: FontWeight.bold, // Bold
+                                color: Color(0xFF414254), // Color #414254
+                              ),
+                            ),
+                            SizedBox(height: 8.0),
+                            Text(
+                              'Wir freuen uns, dich auf deiner Reise zu einer stärkeren Beziehung zu begleiten.',
+                              style: TextStyle(
+                                fontFamily: 'Inter', // Specify the font family
+                                fontSize: 17.0, // Size 17
+                                fontWeight: FontWeight.normal, // Regular weight
+                                color: Color(0xFF414254), // Color #414254
+                              ),
+                            ),
+                            SizedBox(height: 16.0),
+                            Row(
+  children: <Widget>[
+    // Selected indicator
+    Container(
+      width: 30.0,
+      height: 10.0,
+      decoration: BoxDecoration(
+        color: Color(0xFF7D4666), // Selected color
+        borderRadius: BorderRadius.circular(6.0),
+      ),
+    ),
+    SizedBox(width: 4.0),
+    // Non-selected indicators
+    Row(
+      children: List.generate(3, (index) {
+        return Container(
+          margin: EdgeInsets.symmetric(horizontal: 2.0),
+          width: 10,
+          height: 10.0,
+          decoration: BoxDecoration(
+            color: Color(0xFF414254), // Non-selected color
+            shape: BoxShape.circle,
+          ),
+        );
+      }),
+    ),
+    Spacer(),
+  ],
+)
+
+                          ],
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 16.0,
+                        right: 16.0,
+                        child: GestureDetector(
+                          onTap: () {
+                            // Handle navigation action
+                          },
+                          child: Icon(
+                            Icons.arrow_forward,
+                            size: 50,
+                            color: Color(0xFF414254), // Color #414254
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                // Gradient overlay with 20% opacity covering entire screen
                 Positioned.fill(
                   child: Container(
                     decoration: BoxDecoration(
@@ -28,21 +165,6 @@ class FirstPage extends StatelessWidget {
                           Color(0xFFF9AC36).withOpacity(0.2), // Bottom color with 20% opacity
                         ],
                         stops: [0.0, 1.0],
-                      ),
-                    ),
-                  ),
-                ),
-                // Add your other UI components here
-                Center(
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: constraints.maxWidth * 0.1),
-                    child: Text(
-                      'Welcome to the App!',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: constraints.maxWidth * 0.06, // Responsive font size
-                        fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
