b87fc4b728cd0fe718e3814cc7df977409fe31e1
Simon Peter <petersim@student.ethz.ch>
2024-06-04 22:52:26 +0200

    registrieren front end works

--- lib/registration_page.dart
+++ lib/registration_page.dart
@@ -1,133 +1,194 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
 
-class RegistrationPage extends StatelessWidget {
+class RegistrationPage extends StatefulWidget {
+  @override
+  _RegistrationPageState createState() => _RegistrationPageState();
+}
+
+class _RegistrationPageState extends State<RegistrationPage> {
+  bool _isPasswordVisible = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.all(24.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              SizedBox(height: 40), // Add some space at the top
-              Text(
-                'Registrieren',
-                style: TextStyle(
-                  fontSize: 32,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xFF414254),
-                ),
-              ),
-              SizedBox(height: 40), // Add space before the text fields
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12.0),
-                  ),
-                  filled: true,
-                  fillColor: Colors.grey[200],
-                  hintText: 'yourusername@domain.com',
-                ),
-              ),
-              SizedBox(height: 20),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12.0),
-                  ),
-                  filled: true,
-                  fillColor: Colors.grey[200],
-                  hintText: 'Enter your password',
-                  suffixIcon: Icon(Icons.visibility_off),
-                ),
-              ),
-              SizedBox(height: 30),
-              SizedBox(
-                width: double.infinity,
-                height: 56,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xFF7D4666),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
-                  ),
-                  onPressed: () {
-                    // Handle registration
-                  },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Registrieren',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(width: 10),
-                      Icon(Icons.arrow_forward),
-                    ],
-                  ),
-                ),
-              ),
-              SizedBox(height: 30),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      // Handle Google sign-in
-                    },
-                    child: Image.asset(
-                      'assets/graphics/google_icon.png',
-                      width: 50,
-                      height: 50,
-                    ),
-                  ),
-                  SizedBox(width: 20),
-                  GestureDetector(
-                    onTap: () {
-                      // Handle Facebook sign-in
-                    },
-                    child: Image.asset(
-                      'assets/graphics/facebook_icon.png',
-                      width: 50,
-                      height: 50,
-                    ),
-                  ),
-                  SizedBox(width: 20),
-                  GestureDetector(
-                    onTap: () {
-                      // Handle Apple sign-in
-                    },
-                    child: Image.asset(
-                      'assets/graphics/apple_icon.png',
-                      width: 50,
-                      height: 50,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 30),
-              Center(
-                child: GestureDetector(
-                  onTap: () {
-                    // Handle navigation to login
-                  },
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(height: 80), // Add some space at the top
+                Align(
+                  alignment: Alignment.center,
                   child: Text(
-                    'Sie haben ein Konto? Anmelden',
+                    'Registrieren',
                     style: TextStyle(
-                      color: Color(0xFF7FCCB1),
-                      fontSize: 16,
-                      fontWeight: FontWeight.normal,
+                      fontFamily: 'Inter',
+                      fontSize: 48,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF414254), // Color from the image
                     ),
                   ),
                 ),
-              ),
-            ],
+                SizedBox(height: 40), // Add space before the text fields
+                TextField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    filled: true,
+                    fillColor:
+                        Color(0xFFF7F7F7), // Background color from the image
+                    hintText: 'yourusername@domain.com',
+                    hintStyle: TextStyle(
+                      fontFamily: 'Inter',
+                      fontSize: 17,
+                      color: Color(0xFFB2B2B2), // Text color from the image
+                    ),
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+                  ),
+                ),
+                SizedBox(height: 20),
+
+                TextField(
+                  obscureText: !_isPasswordVisible,
+                  decoration: InputDecoration(
+                    border: InputBorder.none, // No border
+                    filled: true,
+                    fillColor: Color(0xFFF7F7F7), // Background color
+                    hintText: 'Enter your password',
+                    hintStyle: TextStyle(
+                      fontFamily: 'Inter',
+                      fontSize: 17,
+                      color: Color(0xFFB2B2B2), // Text color from the image
+                    ),
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+                    suffixIcon: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _isPasswordVisible = !_isPasswordVisible;
+                        });
+                      },
+                      child: Icon(
+                        _isPasswordVisible
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                        color: Color(0xFFB2B2B2), // Icon color matching text color
+                      ),
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 30),
+                SizedBox(
+                  width: double.infinity,
+                  height: 56,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xFF7D4666),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                    ),
+                    onPressed: () {
+                      // Handle registration
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Registrieren',
+                          style: TextStyle(
+                            fontFamily: 'Inter',
+                            fontSize: 17,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.white,
+                            height: 1.41,
+                            letterSpacing: -0.5,
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Icon(
+                          Icons.arrow_forward,
+                          color: Colors.white,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        // Handle Google sign-in
+                      },
+                      child: Image.asset(
+                        'assets/graphics/google_icon.png',
+                        width: 50,
+                        height: 50,
+                      ),
+                    ),
+                    SizedBox(width: 20),
+                    GestureDetector(
+                      onTap: () {
+                        // Handle Facebook sign-in
+                      },
+                      child: Image.asset(
+                        'assets/graphics/facebook_icon.png',
+                        width: 50,
+                        height: 50,
+                      ),
+                    ),
+                    SizedBox(width: 20),
+                    GestureDetector(
+                      onTap: () {
+                        // Handle Apple sign-in
+                      },
+                      child: Image.asset(
+                        'assets/graphics/apple_icon.png',
+                        width: 50,
+                        height: 50,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 30),
+                Center(
+                  child: RichText(
+                    text: TextSpan(
+                      text: 'Sie haben ein Konto? ',
+                      style: TextStyle(
+                        color: Color(
+                            0xFF757575), // The grey color for the first part
+                        fontSize: 16,
+                        fontFamily: 'Inter',
+                        fontWeight: FontWeight.normal,
+                      ),
+                      children: [
+                        TextSpan(
+                          text: 'Anmelden',
+                          style: TextStyle(
+                            color: Color(
+                                0xFF7FCCB1), // The green color for the clickable part
+                            fontSize: 16,
+                            fontFamily: 'Inter',
+                            fontWeight: FontWeight.normal,
+                          ),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              // Handle navigation to login
+                            },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
