322b118c48f4dc5128d30756cf9010c57ed307a3
Simon Peter <petersim@student.ethz.ch>
2024-06-04 21:50:21 +0200

    added background blurr to white box

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'onboarding_page.dart'; // Import the OnboardingPage
+import 'dart:ui';
 
 class FirstPage extends StatefulWidget {
   @override
@@ -85,8 +86,11 @@ class _FirstPageState extends State<FirstPage> with TickerProviderStateMixin {
   void _setRandomTransformations(int index) {
     if (_flipHorizontally.length <= index) {
       _flipHorizontally.add(random.nextBool());
-      _rotationAngles.add((random.nextDouble() - 0.5) * pi / 3); // Rotate between -60 to 60 degrees
-      _scaleFactors.add(0.8 + random.nextDouble() * 0.4); // Scale between 0.8 and 1.2
+      _rotationAngles.add((random.nextDouble() - 0.5) *
+          pi /
+          3); // Rotate between -60 to 60 degrees
+      _scaleFactors
+          .add(0.8 + random.nextDouble() * 0.4); // Scale between 0.8 and 1.2
     } else {
       _flipHorizontally[index] = random.nextBool();
       _rotationAngles[index] = (random.nextDouble() - 0.5) * pi / 3;
@@ -127,23 +131,32 @@ class _FirstPageState extends State<FirstPage> with TickerProviderStateMixin {
                 // Animated SVG leaves
                 for (int i = 0; i < _leavesControllers.length; i++)
                   Positioned(
-                    top: constraints.maxHeight * (0.05 + i * 0.1), // Different positions for different sets of leaves
+                    top: constraints.maxHeight *
+                        (0.05 +
+                            i * 0.1), // Different positions for different sets of leaves
                     left: 0,
                     right: 0,
                     child: AnimatedBuilder(
                       animation: _leavesControllers[i],
                       builder: (context, child) {
                         return Transform.translate(
-                          offset: _leavesAnimations[i].value * constraints.maxWidth,
+                          offset:
+                              _leavesAnimations[i].value * constraints.maxWidth,
                           child: Transform(
                             alignment: Alignment.center,
                             transform: Matrix4.identity()
-                              ..scale(_flipHorizontally[i] ? -_scaleFactors[i] : _scaleFactors[i], _scaleFactors[i])
+                              ..scale(
+                                  _flipHorizontally[i]
+                                      ? -_scaleFactors[i]
+                                      : _scaleFactors[i],
+                                  _scaleFactors[i])
                               ..rotateZ(_rotationAngles[i]),
                             child: SvgPicture.asset(
                               'assets/graphics/freepik--Leaves.svg',
-                              width: constraints.maxWidth * 1, // Adjust size as needed
-                              height: constraints.maxHeight * 0.2, // Adjust size as needed
+                              width: constraints.maxWidth *
+                                  1, // Adjust size as needed
+                              height: constraints.maxHeight *
+                                  0.2, // Adjust size as needed
                             ),
                           ),
                         );
@@ -152,7 +165,8 @@ class _FirstPageState extends State<FirstPage> with TickerProviderStateMixin {
                   ),
                 // Centered SVG logo with fade-in effect
                 Positioned(
-                  top: constraints.maxHeight * 0.31, // Adjust the top position as needed
+                  top: constraints.maxHeight *
+                      0.31, // Adjust the top position as needed
                   left: 0,
                   right: 0,
                   child: AnimatedOpacity(
@@ -160,8 +174,10 @@ class _FirstPageState extends State<FirstPage> with TickerProviderStateMixin {
                     duration: Duration(seconds: 2),
                     child: SvgPicture.asset(
                       'assets/graphics/logo_white.svg',
-                      width: constraints.maxWidth * 0.4, // Adjust size as needed
-                      height: constraints.maxHeight * 0.15, // Adjust size as needed
+                      width:
+                          constraints.maxWidth * 0.4, // Adjust size as needed
+                      height:
+                          constraints.maxHeight * 0.15, // Adjust size as needed
                     ),
                   ),
                 ),
@@ -173,8 +189,10 @@ class _FirstPageState extends State<FirstPage> with TickerProviderStateMixin {
                         begin: Alignment.topCenter,
                         end: Alignment.bottomCenter,
                         colors: [
-                          Color(0xFF22577A).withOpacity(0.2), // Top color with 20% opacity
-                          Color(0xFFF9AC36).withOpacity(0.2), // Bottom color with 20% opacity
+                          Color(0xFF22577A)
+                              .withOpacity(0.2), // Top color with 20% opacity
+                          Color(0xFFF9AC36).withOpacity(
+                              0.2), // Bottom color with 20% opacity
                         ],
                         stops: [0.0, 1.0],
                       ),
@@ -190,69 +208,94 @@ class _FirstPageState extends State<FirstPage> with TickerProviderStateMixin {
                     position: _slideAnimation,
                     child: Stack(
                       children: [
-                        Container(
-                          padding: EdgeInsets.fromLTRB(24.0, 40.0, 24.0, 70.0), // Add padding: left, top, right, bottom
-                          decoration: BoxDecoration(
-                            color: Colors.white.withOpacity(0.8), // Slightly transparent white
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(24.0),
-                              topRight: Radius.circular(24.0),
-                            ),
+                        ClipRRect(
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(24.0),
+                            topRight: Radius.circular(24.0),
                           ),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text(
-                                'Willkommen!',
-                                style: TextStyle(
-                                  fontFamily: 'Inter', // Specify the font family
-                                  fontSize: 48.0, // Size 48
-                                  fontWeight: FontWeight.bold, // Bold
-                                  color: Color(0xFF414254), // Color #414254
+                          child: BackdropFilter(
+                            filter: ImageFilter.blur(
+                                sigmaX: 5,
+                                sigmaY: 5), // Apply background blur
+                            child: Container(
+                              padding: EdgeInsets.fromLTRB(24.0, 40.0, 24.0,
+                                  70.0), // Add padding: left, top, right, bottom
+                              decoration: BoxDecoration(
+                                color: Colors.white.withOpacity(
+                                    0.55), // Slightly transparent white fill with 55% opacity
+                                border: Border.all(
+                                  color: Colors.white.withOpacity(
+                                      0.3), // Border with 30% opacity
+                                  width: 1.0, // Adjust the width as needed
+                                ),
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(24.0),
+                                  topRight: Radius.circular(24.0),
                                 ),
                               ),
-                              SizedBox(height: 8.0),
-                              Text(
-                                'Wir freuen uns, dich auf deiner Reise zu einer stärkeren Beziehung zu begleiten.',
-                                style: TextStyle(
-                                  fontFamily: 'Inter', // Specify the font family
-                                  fontSize: 17.0, // Size 17
-                                  fontWeight: FontWeight.normal, // Regular weight
-                                  color: Color(0xFF414254), // Color #414254
-                                ),
-                              ),
-                              SizedBox(height: 32.0),
-                              Row(
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                crossAxisAlignment: CrossAxisAlignment.start,
                                 children: <Widget>[
-                                  // Selected indicator
-                                  Container(
-                                    width: 30.0,
-                                    height: 10.0,
-                                    decoration: BoxDecoration(
-                                      color: Color(0xFF7D4666), // Selected color
-                                      borderRadius: BorderRadius.circular(6.0),
+                                  Text(
+                                    'Willkommen!',
+                                    style: TextStyle(
+                                      fontFamily:
+                                          'Inter', // Specify the font family
+                                      fontSize: 48.0, // Size 48
+                                      fontWeight: FontWeight.bold, // Bold
+                                      color: Color(0xFF414254), // Color #414254
                                     ),
                                   ),
-                                  SizedBox(width: 4.0),
-                                  // Non-selected indicators
+                                  SizedBox(height: 8.0),
+                                  Text(
+                                    'Wir freuen uns, dich auf deiner Reise zu einer stärkeren Beziehung zu begleiten.',
+                                    style: TextStyle(
+                                      fontFamily:
+                                          'Inter', // Specify the font family
+                                      fontSize: 17.0, // Size 17
+                                      fontWeight:
+                                          FontWeight.normal, // Regular weight
+                                      color: Color(0xFF414254), // Color #414254
+                                    ),
+                                  ),
+                                  SizedBox(height: 32.0),
                                   Row(
-                                    children: List.generate(6, (index) {
-                                      return Container(
-                                        margin: EdgeInsets.symmetric(horizontal: 2.0),
-                                        width: 10,
+                                    children: <Widget>[
+                                      // Selected indicator
+                                      Container(
+                                        width: 30.0,
                                         height: 10.0,
                                         decoration: BoxDecoration(
-                                          color: Color(0xFF414254), // Non-selected color
-                                          shape: BoxShape.circle,
+                                          color: Color(
+                                              0xFF7D4666), // Selected color
+                                          borderRadius:
+                                              BorderRadius.circular(6.0),
                                         ),
-                                      );
-                                    }),
+                                      ),
+                                      SizedBox(width: 4.0),
+                                      // Non-selected indicators
+                                      Row(
+                                        children: List.generate(6, (index) {
+                                          return Container(
+                                            margin: EdgeInsets.symmetric(
+                                                horizontal: 2.0),
+                                            width: 10,
+                                            height: 10.0,
+                                            decoration: BoxDecoration(
+                                              color: Color(
+                                                  0xFF414254), // Non-selected color
+                                              shape: BoxShape.circle,
+                                            ),
+                                          );
+                                        }),
+                                      ),
+                                      Spacer(),
+                                    ],
                                   ),
-                                  Spacer(),
                                 ],
                               ),
-                            ],
+                            ),
                           ),
                         ),
                         Positioned(
@@ -263,7 +306,8 @@ class _FirstPageState extends State<FirstPage> with TickerProviderStateMixin {
                               // Handle navigation action
                               Navigator.push(
                                 context,
-                                MaterialPageRoute(builder: (context) => OnboardingPage()),
+                                MaterialPageRoute(
+                                    builder: (context) => OnboardingPage()),
                               );
                             },
                             child: Container(
