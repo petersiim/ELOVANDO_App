3c8522ac9b84d48208080a1e8ad779357bbf213b
Simon Peter <petersim@student.ethz.ch>
2024-06-21 15:46:24 +0200

    animations working but bnot only inside the image

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,8 +1,8 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'custom_app_bar.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'dart:math';
+import 'custom_app_bar.dart';
 
 class HomePage extends StatefulWidget {
   final String userId;
@@ -14,56 +14,77 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
-  late AnimationController _leavesController;
-  late Animation<Offset> _leavesAnimation;
-  late bool _flipHorizontally;
-  late double _rotationAngle;
-  late double _scaleFactor;
+  late Future<DocumentSnapshot> userFuture;
+
+  final List<AnimationController> _leavesControllers = [];
+  final List<Animation<Offset>> _leavesAnimations = [];
+  final List<bool> _flipHorizontally = [];
+  final List<double> _rotationAngles = [];
+  final List<double> _scaleFactors = [];
   final Random random = Random();
 
   @override
   void initState() {
     super.initState();
-    _initializeLeavesController();
-    _setRandomTransformations();
-    _leavesController.forward();
+    userFuture = FirebaseFirestore.instance.collection('users').doc(widget.userId).get();
+    _initializeLeavesControllers();
+    _startLeavesAnimations();
   }
 
-  void _initializeLeavesController() {
-    _leavesController = AnimationController(
-      vsync: this,
-      duration: Duration(seconds: 5),
-    );
+  void _initializeLeavesControllers() {
+    for (int i = 0; i < 3; i++) {
+      final controller = AnimationController(
+        vsync: this,
+        duration: Duration(seconds: 5),
+      );
 
-    _leavesAnimation = Tween<Offset>(
-      begin: Offset(0.1, 0), // Start just off the left side
-      end: Offset(-0.1, 0), // End just off the right side
-    ).animate(CurvedAnimation(
-      parent: _leavesController,
-      curve: Curves.linear,
-    ));
-
-    _leavesController.addStatusListener((status) {
-      if (status == AnimationStatus.completed) {
-        setState(() {
-          _setRandomTransformations();
-          Future.delayed(Duration(seconds: random.nextInt(2)), () {
-            _leavesController.forward(from: 0);
+      controller.addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          setState(() {
+            _setRandomTransformations(i);
+            Future.delayed(Duration(seconds: random.nextInt(2)), () {
+              controller.forward(from: 0);
+            });
           });
-        });
-      }
-    });
+        }
+      });
+
+      _leavesControllers.add(controller);
+      _leavesAnimations.add(Tween<Offset>(
+        begin: Offset(1, 0),
+        end: Offset(-1, 0),
+      ).animate(CurvedAnimation(
+        parent: controller,
+        curve: Curves.linear,
+      )));
+
+      _setRandomTransformations(i);
+    }
   }
 
-  void _setRandomTransformations() {
-    _flipHorizontally = random.nextBool();
-    _rotationAngle = (random.nextDouble() - 0.5) * pi / 3; // Rotate between -60 to 60 degrees
-    _scaleFactor = 0.8 + random.nextDouble() * 0.4; // Scale between 0.8 and 1.2
+  void _setRandomTransformations(int index) {
+    if (_flipHorizontally.length <= index) {
+      _flipHorizontally.add(random.nextBool());
+      _rotationAngles.add((random.nextDouble() - 0.5) * pi / 3); // Rotate between -60 to 60 degrees
+      _scaleFactors.add(0.8 + random.nextDouble() * 0.4); // Scale between 0.8 and 1.2
+    } else {
+      _flipHorizontally[index] = random.nextBool();
+      _rotationAngles[index] = (random.nextDouble() - 0.5) * pi / 3;
+      _scaleFactors[index] = 0.9 + random.nextDouble() * 0.5;
+    }
+  }
+
+  void _startLeavesAnimations() {
+    for (int i = 0; i < _leavesControllers.length; i++) {
+      Future.delayed(Duration(milliseconds: random.nextInt(1000)), () {
+        _leavesControllers[i].forward();
+      });
+    }
   }
 
   @override
   void dispose() {
-    _leavesController.dispose();
+    _leavesControllers.forEach((controller) => controller.dispose());
     super.dispose();
   }
 
@@ -73,7 +94,7 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
       backgroundColor: Color(0xFFF7F7F7),
       appBar: CustomAppBar(userId: widget.userId),
       body: FutureBuilder<DocumentSnapshot>(
-        future: FirebaseFirestore.instance.collection('users').doc(widget.userId).get(),
+        future: userFuture,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return Center(child: CircularProgressIndicator());
@@ -153,27 +174,31 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
                         ),
                       ),
                       Positioned.fill(
-                        child: AnimatedBuilder(
-                          animation: _leavesController,
-                          builder: (context, child) {
-                            return Transform.translate(
-                              offset: _leavesAnimation.value * MediaQuery.of(context).size.width,
-                              child: Transform(
-                                alignment: Alignment.center,
-                                transform: Matrix4.identity()
-                                  ..scale(
-                                    _flipHorizontally ? -_scaleFactor : _scaleFactor,
-                                    _scaleFactor,
-                                  )
-                                  ..rotateZ(_rotationAngle),
-                                child: SvgPicture.asset(
-                                  'assets/graphics/home_love_session_starten_animation_graphic.svg',
-                                  width: MediaQuery.of(context).size.width * 0.6,
-                                  height: MediaQuery.of(context).size.height * 0.3,
-                                ),
-                              ),
+                        child: Stack(
+                          children: List.generate(_leavesControllers.length, (index) {
+                            return AnimatedBuilder(
+                              animation: _leavesControllers[index],
+                              builder: (context, child) {
+                                return Transform.translate(
+                                  offset: _leavesAnimations[index].value * MediaQuery.of(context).size.width,
+                                  child: Transform(
+                                    alignment: Alignment.center,
+                                    transform: Matrix4.identity()
+                                      ..scale(
+                                        _flipHorizontally[index] ? -_scaleFactors[index] : _scaleFactors[index],
+                                        _scaleFactors[index],
+                                      )
+                                      ..rotateZ(_rotationAngles[index]),
+                                    child: SvgPicture.asset(
+                                      'assets/graphics/home_love_session_starten_animation_graphic.svg',
+                                      width: MediaQuery.of(context).size.width * 0.6,
+                                      height: MediaQuery.of(context).size.height * 0.3,
+                                    ),
+                                  ),
+                                );
+                              },
                             );
-                          },
+                          }),
                         ),
                       ),
                     ],
