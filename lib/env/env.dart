df42dcf9feb69ffa01b116fbfbae91288036e9db
Simon Peter <petersim@student.ethz.ch>
2024-09-03 21:36:46 +0200

    newest commit

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -1,9 +1,8 @@
-// lib/env/env.dart
 import 'package:envied/envied.dart';
+
 part 'env.g.dart';
 
-@Envied(path: ".env")
+@Envied(path: '.env')
 abstract class Env {
-  @EnviedField(varName: 'OPEN_AI_API_KEY') // the .env variable.
-  static const String apiKey = _Env.apiKey;
-}
+  @EnviedField(varName: 'API_KEY')
+  static const apiKey = _Env.apiKey;}
