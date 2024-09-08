7181c31a3bb7aa35c5e936094e6a4a2f199c11ef
Simon Peter <petersim@student.ethz.ch>
2024-09-08 16:03:21 +0200

    changes

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -1,5 +1,16 @@
+import 'package:envied/envied.dart';
 import 'dart:io';
 
+part 'env.g.dart';
+
+// Uncomment the following lines for local testing
+// @Envied(path: '.env')
+// abstract class Env {
+//   @EnviedField(varName: 'API_KEY')
+//   static const String apiKey = _Env.apiKey;
+// }
+
+// Use this for production builds
 class Env {
-  static String get apiKey => Platform.environment['OPEN_AI_API_KEY'] ?? '';
+  static String get apiKey => Platform.environment['API_KEY'] ?? '';
 }
